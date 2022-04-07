6547bac2c47b73bf898640b0f7fb34b664fe5902
Bhaskar Dharamsoth <bhaskar.dharamsoth@ATMECS.CORP>
2022-04-07 10:33:03 +0530

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BusinessComponent } from './business/business.component';
 import { CartComponent } from './component/cart/cart.component';
 import { LogoutPageComponent } from './component/logout-page/logout-page.component';
 import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
@@ -8,10 +9,17 @@ import { RegisterComponent } from './component/register/register.component';
 import { DirectivesComponent } from './directives/directives.component';
 import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
 import { ProductComponent } from './product/product.component';
+import { ScienceComponent } from './science/science.component';
 import { HomeComponent } from './shared/home/home.component';
+import { TechComponent } from './tech/tech.component';
+import { TopHeadlineComponent } from './top-headline/top-headline.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'products',pathMatch:'full'},
+  {path: 'headline', component:TopHeadlineComponent},
+  {path: 'tech-news', component:TechComponent},
+  {path: 'business-news', component:BusinessComponent},
+  {path: 'science', component:ScienceComponent},
   {path:'products', component:ProductsComponent},
   {path:'cart', component:CartComponent},
   {path:'product', component:ProductComponent},
@@ -20,6 +28,7 @@ const routes: Routes = [
   {path: 'logout', component:LogoutPageComponent},
   {path: 'register', component:RegisterComponent},
   {path: 'employees', component:EmployeeDashboardComponent},
+  
   {path: '**', component:PageNotFoundComponent}
 ];
 
